4fc53cca7f91d16403a468f01442d74b1ca6396e
ofabiano1x <86899853+OFabiano1@users.noreply.github.com>
2025-04-21 03:36:30 -0300

    add hexe!
    feito pelo marketas obrigado

---
+++ axolotl.hx
@@ -0,0 +1,18 @@
+package;
+
+/**
+ * Classe de impressão simples em Haxe.
+ */
+class Axolotl
+{
+    /**
+     * Classe de carregamento principal da máquina virtual do código.
+     */
+    static public function main(  ):Void
+    {
+        trace( "Axolotl!" );
+    }
+}
+
+* Feito por Marketas
+* Obrigado!!
